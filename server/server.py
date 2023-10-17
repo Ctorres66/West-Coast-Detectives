7bf2691039baf16c6b6d76db801c6da1014c3d95
Ctorres66 <ctorres4820021@gmail.com>
2023-10-17 04:41:35 -0400

    connections between client and server attempted, needs fixing

--- server/server.py
+++ server/server.py
@@ -16,34 +16,55 @@ except socket.error as e:
     str(e)
 
 # 6 is the maximum player # for Clue-Less
-s.listen(6)
+s.listen(2)
 print("Waiting for a Connection, Server Started")
 
-def threaded_client(conn):
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + " , " + str(tup[1])
+
+
+pos = [(0,0), (100,100)]
+
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
-
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Recieved : ", reply)
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Recieved : ", data)
                 print("Sending  : ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
         except:
             break
 
-    pass
+    print("Lost Connection")
+    conn.close()
 
 
+currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to ", addr)
+    start_new_thread(threaded_client, (conn,currentPlayer))
+    currentPlayer += 1
 
-    start_new_thread(threaded_client, (conn,))
 
 
