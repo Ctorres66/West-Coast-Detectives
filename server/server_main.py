6d2322dc6cfcadd7628448d44550619ec229b3d1
Ctorres66 <ctorres4820021@gmail.com>
2023-12-10 19:31:01 -0800

    text boxes made for suggestion and accusation buttons

--- server/server_main.py
+++ server/server_main.py
@@ -67,7 +67,7 @@ class Server:
                     elif action_data.get('action') == 'accusation':
                         self.game_engine.handle_accusation_action(player_id, action_data.get('room'), action_data.get('suspect'), action_data.get('weapon'))
                     elif action_data.get('action') == 'suggestion':
-                        self.game_engine.handle_accusation_action(player_id, action_data.get('room'), action_data.get('suspect'), action_data.get('weapon'))
+                        self.game_engine.handle_suggestion_action(player_id, action_data.get('room'), action_data.get('suspect'), action_data.get('weapon'))
                     elif action_data.get('action') == 'skip_player':
                         self.game_engine.next_turn()
                         self.game_engine.update_game_state()
